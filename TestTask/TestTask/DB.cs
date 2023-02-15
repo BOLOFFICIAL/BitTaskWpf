ccf0422f5380fb386839ae5d752b391bf40077db
Bolonin  Official <boloninnn@gmail.com>
2023-02-16 01:06:59 +0300

    Update

--- TestTask/TestTask/DB.cs
+++ TestTask/TestTask/DB.cs
@@ -11,21 +11,21 @@ namespace TestTask
         {
             sql = new SqlConnection(@$"Data Source=PC-BOLOFFICIAL\SQLEXPRESS;Initial Catalog={name};Integrated Security=True");
         }
-        public void OpenConnection()
-        {
-            if (sql.State == System.Data.ConnectionState.Closed)
-            {
-                sql.Open();
-            }
-        }
+        //public void OpenConnection()
+        //{
+        //    if (sql.State == System.Data.ConnectionState.Closed)
+        //    {
+        //        sql.Open();
+        //    }
+        //}
 
-        public void CloseConnection()
-        {
-            if (sql.State == System.Data.ConnectionState.Open)
-            {
-                sql.Close();
-            }
-        }
+        //public void CloseConnection()
+        //{
+        //    if (sql.State == System.Data.ConnectionState.Open)
+        //    {
+        //        sql.Close();
+        //    }
+        //}
 
         public SqlConnection GetConnection() 
         {
